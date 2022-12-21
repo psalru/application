b0a7a321e60a23914cd1ab9b8c1a2ebd46e83752
Alexey Safonov <alexey@safonov.pro>
2022-12-21 19:44:34 +0300

    refactor: немного красоты в админке

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import NewsFeed
 
-# Register your models here.
+
+class NewsFeedAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'domain', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title')
+
+
+admin.site.register(NewsFeed, NewsFeedAdmin)
