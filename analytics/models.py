6816933e1ac96cb12f3c912154a221eb2bca1dd9
Alexey Safonov <alexey@safonov.pro>
2022-12-21 18:43:02 +0300

    feat: табличка для новостного потока

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModels(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    class Meta:
+        abstract = True
+
+
+class NewsFeed(BaseModels):
+    keyword = models.TextField(help_text='Ключевое слово по которому ищет Google Alerts')
+    date = models.DateField(help_text='Дата публикации')
+    domain = models.TextField(help_text='Домен источника')
+    url = models.TextField(unique=True, help_text='Ссылка на первоисточник')
+    title = models.TextField(help_text='Заголовок')
+    content = models.TextField(help_text='Содержание')
+    s3_bucket = models.TextField(help_text='Баккет в S3')
+    s3_key = models.TextField(help_text='Путь к HTML-контенту в S3')
+
+    def __str__(self) -> str:
+        return self.title
