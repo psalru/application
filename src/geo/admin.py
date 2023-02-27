37e791430a80576dad3769bc0dff759dad7a828e
Alexey Safonov <alexey@safonov.pro>
2023-02-27 21:07:24 +0300

    feat: первая ревизия структур данных под геообъекты

--- src/geo/admin.py
+++ src/geo/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class FederalDistrictAdmin(admin.ModelAdmin):
+    list_display = ['id', 'short_title', 'title', 'capital', 'square']
+    list_display_links = ['id', 'title', 'short_title']
+    search_fields = ['title', 'short_title', 'capital']
+    ordering = ('title',)
+
+
+class FederalRegionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type', 'title', 'capital', 'square']
+    list_display_links = ['id', 'title']
+    search_fields = ['title', 'capital']
+    ordering = ('title',)
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['title', 'federal_district', 'federal_region']
+    search_fields = ['title']
+    ordering = ('title',)
+
+
+admin.site.register(FederalDistrict, FederalDistrictAdmin)
+admin.site.register(FederalRegion, FederalRegionAdmin)
+admin.site.register(City, CityAdmin)
--- src/geo/models.py
+++ src/geo/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from config.models import BaseModel
 
-# Create your models here.
+
+class FederalDistrict(BaseModel):
+    title = models.CharField(max_length=255, help_text='Название федерального округа')
+    short_title = models.CharField(max_length=255, help_text='Краткое название федерального округа')
+    capital = models.ForeignKey('City', null=True, help_text='Столица федерального округа', on_delete=models.PROTECT)
+    square = models.FloatField(help_text='Площадь федерального округа')
+
+    def __str__(self):
+        return self.short_title
+
+    class Meta:
+        db_table = 'geo_federal_district'
+
+
+class FederalRegion(BaseModel):
+    class FederalRegionType(models.TextChoices):
+        REPUBLIC = 'republic', 'Республика'
+        EDGE = 'edge', 'Край'
+        AREA = 'area', 'Область'
+        CITY = 'city', 'Город федерального значения'
+        REGION = 'region', 'Регион'
+        OKRUG = 'okrug', 'Автономный округ'
+
+    title = models.CharField(max_length=255, help_text='Название региона')
+    type = models.CharField(max_length=27, choices=FederalRegionType.choices, help_text='Тип региона')
+    capital = models.ForeignKey('City', null=True, help_text='Столица региона', on_delete=models.PROTECT)
+    square = models.FloatField(help_text='Площадь региона')
+    okato = models.IntegerField(help_text='ОКАТО')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'geo_federal_region'
+
+
+class City(BaseModel):
+    title = models.CharField(max_length=255, help_text='Название города')
+    federal_district = models.ForeignKey(FederalDistrict, help_text='Федеральный округ', on_delete=models.PROTECT)
+    federal_region = models.ForeignKey(FederalRegion, help_text='Регион', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Cities'
