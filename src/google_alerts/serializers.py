27f6db521612452317bb8ccfba95f4f11dba3646
Alexey Safonov <alexey@safonov.pro>
2023-03-26 19:48:53 +0300

    refactor: сразу ссылка на S3

--- src/google_alerts/serializers.py
+++ src/google_alerts/serializers.py
@@ -4,6 +4,11 @@ from .models import NewsFeed
 
 
 class NewsFeedSerializer(serializers.ModelSerializer):
+    s3 = serializers.SerializerMethodField('get_s3')
+
     class Meta:
         model = NewsFeed
-        fields = '__all__'
+        exclude = ['s3_bucket', 's3_key']
+
+    def get_s3(self, obj):
+        return f"https://storage.yandexcloud.net/{obj.s3_bucket}/{obj.s3_key}" if obj.s3_key else None
--- src/hh/serializers.py
+++ src/hh/serializers.py
@@ -4,9 +4,13 @@ from .models import Vacancy
 
 
 class VacancyFeedSerializer(serializers.ModelSerializer):
+    s3 = serializers.SerializerMethodField('get_s3')
     professional_role = serializers.StringRelatedField(many=True)
     skill = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Vacancy
-        fields = '__all__'
+        exclude = ['s3_bucket', 's3_key']
+
+    def get_s3(self, obj):
+        return f"https://storage.yandexcloud.net/{obj.s3_bucket}/{obj.s3_key}" if obj.s3_key else None
